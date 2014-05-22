e269f4d1db18db14baf0c0360704cf8fef52d639
James Hart & Paul Henry <james+paul@wanelo.com>
2014-05-21 17:31:35 -0700

    Correctly parse fqdn out of node

--- recipes/a_record.rb
+++ recipes/a_record.rb
@@ -4,7 +4,7 @@ managed_dns_record node['fqdn'] do
   domain node['dynect']['zone']
   record_type 'A'
   ipaddress node['privateaddress']
-  a_record node['fqdn'].split('.').first
+  a_record node['fqdn'].split('.')[0..-2].join(".")
   ttl 60
   action :create
 end
