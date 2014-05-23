e427adea285baa8320c81a214e47907bdc79ab13
Richard Millan <richardiux@gmail.com>
2014-05-23 14:38:10 -0700

    Managed DNS record uses hostname instead of fqdn

--- recipes/a_record.rb
+++ recipes/a_record.rb
@@ -1,10 +1,10 @@
 include_recipe 'managed-dns::default'
 
-managed_dns_record node['fqdn'] do
+managed_dns_record node['hostname'] do
   domain node['dynect']['zone']
   record_type 'A'
   ipaddress node['privateaddress']
-  a_record node['fqdn'].split('.')[0..-2].join(".")
+  a_record node['hostname'].split('.')[0..-2].join(".")
   ttl 60
   action :create
 end
